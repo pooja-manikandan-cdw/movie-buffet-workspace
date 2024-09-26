20dc333d4182bf23423de70a50184f83c3bb5d0a
Pooja-Manikandann <Pooja.Manikandan@cdw.com>
2024-09-27 00:27:41 +0530

    Create responseUtils.js

---
+++ responseUtils.js
@@ -0,0 +1,11 @@
+const setResponse = (res, status, success, error, message, data) => {
+  console.log(status, success, error, message, data);
+  return res.status(status).send({
+    success: success,
+    error: error,
+    message: message,
+    data: data,
+  });
+};
+
+module.exports = { setResponse };
